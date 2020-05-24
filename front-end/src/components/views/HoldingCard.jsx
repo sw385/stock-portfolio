ad3444bba2e92331be1e5b8432ade1537ec2ebc6
Samson Wu <samson.wu89@myhunter.cuny.edu>
2020-05-24 15:47:46 -0400

    add conditional formatting for current price colors

--- front-end/src/components/views/HoldingCard.jsx
+++ front-end/src/components/views/HoldingCard.jsx
@@ -4,13 +4,20 @@ import PropTypes from "prop-types"
 import "../views/HoldingCard.css";
 
 const HoldingCard = (props) => {
+  let colorClass = "gray"
+  if (props.price[1] < props.price[0]) {
+    colorClass = "red"
+  }
+  else if (props.price[1] > props.price[0]) {
+    colorClass = "green"
+  }
   return (
     <div class="holding-card">
       HoldingCard here
       <p>Symbol: {props.data.symbol}</p>
       <p>{props.data.shares} shares</p>
       <p>Open price: ${props.price[0]}</p>
-      <p>Current price: ${props.price[1]}</p>
+      <p class={colorClass}>Current price: ${props.price[1]}</p>
     </div>
   )
 }
