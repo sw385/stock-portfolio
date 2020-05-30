9e0204c25392791eb57d9117d9b838bd14f0cbbd
Samson Wu <samson.wu89@myhunter.cuny.edu>
2020-05-29 23:56:21 -0400

    add route to buy shares of a stock

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const db = require("../db");
+const express = require("express")
+const router = express.Router()
+const db = require("../db")
 
 router.get("/:username/transactions", async function (req, res, next) {
   try {
-    const results = await db.query("SELECT symbol, shares, price, is_buy, datetime FROM transactions WHERE username=$1 ORDER BY datetime DESC", [req.params.username])
+    const results = await db.query(
+      "SELECT symbol, shares, price, is_buy, datetime FROM transactions WHERE username=$1 ORDER BY datetime DESC",
+      [req.params.username]
+    )
     return res.json(results.rows)
   } catch (err) {
     return next(err)
@@ -13,7 +16,10 @@ router.get("/:username/transactions", async function (req, res, next) {
 
 router.get("/:username/portfolio", async function (req, res, next) {
   try {
-    const results = await db.query("SELECT symbol, shares FROM portfolio WHERE username=$1 ORDER BY symbol", [req.params.username])
+    const results = await db.query(
+      "SELECT symbol, shares FROM portfolio WHERE username=$1 ORDER BY symbol",
+      [req.params.username]
+    )
     return res.json(results.rows)
   } catch (err) {
     return next(err)
@@ -25,8 +31,41 @@ router.post("/:username/buy", async function (req, res, next) {
     // if balance > price * shares:
     // add middleware to check this BEFORE executing the changes
     // return the new transaction?
-    const results = await db.query("SELECT * FROM portfolio WHERE username=$1 ORDER BY symbol", [req.params.symbol, req.params.shares, req.params.price])
-    return res.json(results.rows)
+    let oldBalance = await db.query(
+      "SELECT balance from users WHERE username=$1",
+      [req.params.username]
+    )
+    oldBalance = parseFloat(oldBalance.rows[0]["balance"])
+    let newBalance = oldBalance - req.body.shares * req.body.price
+    await db.query("UPDATE users SET balance=$2 WHERE username=$1", [
+      req.params.username,
+      newBalance,
+    ])
+    const transaction = await db.query(
+      "INSERT INTO transactions (username, is_buy, symbol, shares, price) VALUES ($1, True, $2, $3, $4) RETURNING symbol, shares, price, is_buy, datetime",
+      [req.params.username, req.body.symbol, req.body.shares, req.body.price]
+    )
+
+    let oldShares = await db.query(
+      "SELECT shares from portfolio WHERE (username=$1 AND symbol=$2)",
+      [req.params.username, req.body.symbol]
+    )
+    if (oldShares.rows.length == 0) {
+      await db.query(
+        "INSERT INTO portfolio (username, symbol, shares) VALUES ($1, $2, $3)",
+        [req.params.username, req.body.symbol, req.body.shares]
+      )
+    } else {
+      oldShares = parseInt(oldShares.rows[0]["shares"])
+      let newShares = oldShares + parseInt(req.body.shares)
+      console.log(req.params.username, req.body.symbol, newShares)
+      await db.query(
+        "UPDATE portfolio SET shares=$3 WHERE (username=$1 AND symbol=$2)",
+        [req.params.username, req.body.symbol, newShares]
+      )
+    }
+
+    return res.json(transaction.rows[0])
   } catch (err) {
     return next(err)
   }
@@ -86,4 +125,4 @@ router.post("/", async (req, res, next) => {
 });
 */
 
-module.exports = router;
+module.exports = router
